0c34c8e301d080ca88b8688b4c8990f508e0b72b
Bloesum <ewout.boelsums@gmail.com>
2018-09-05 12:54:31 +0200

    added contract

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # B9Lab_Splitter
+
+Considerations:
+- Upgraded Truffle to latest: v4.1.14 (Solidity v0.4.24)
+- Viewing contract balances doesn't make any sense. The sent amount will immidiatelly be split and resent
---
+++ contracts/Splitter.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.4.24;
+
+contract Splitter {
+	bool private alreadyPaid;
+	address private creator;
+	uint public splitterBalance;
+	
+	event logTransfer(address indexed _from, address indexed _to1, address indexed _to2, uint _amount);
+	
+	constructor () public payable {
+	    creator = msg.sender;
+	    splitterBalance = msg.value;
+	}
+	
+	function split(address recipient1, address recipient2) public payable {
+	    uint splitAmount = msg.value / 2;
+	    require(!alreadyPaid);
+	    alreadyPaid = true;
+        recipient1.transfer(splitAmount);
+	    recipient2.transfer(splitAmount);
+	    emit logTransfer(msg.sender, recipient1, recipient2, splitAmount );
+	}
+	
+	function kill() public {
+        if (msg.sender == creator) selfdestruct(msg.sender);
+    }
+    
+    function () public payable {}
+}
